c1d56299aa8232e1ebe2c50d91a57f61a4acff60
wangrenzhen <974993442@qq.com>
2024-09-03 16:02:42 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,216 +1,197 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
-let scene, camera, renderer, controls, galaxy, gui;
 const canvas = ref(null);
+const gii = new GUI();
 
-// todo 场景
+let scene, camera, renderer, textloader;
+let modelsarr = [];
+
+// ! 场景
 scene = new THREE.Scene();
-gui = new GUI({
-  title: "星系控制器",
-});
-scene.add(new THREE.AxesHelper(1));
 
-// todo 相机
+// ! 灯光
+const directional = new THREE.DirectionalLight("#fff", 2);
+directional.position.set(1, 1, 0);
+scene.add(directional);
+
+// ! 相机
+const cameragroup = new THREE.Group();
 camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
+camera.position.set(0, 0, 5);
+cameragroup.add(camera);
+scene.add(cameragroup);
 
-// todo 星系参数
-const parameters = {
-  num: 1000,
-  size: 0.1,
-  radius: 10,
-  branch: 3,
-  spin: 0.5,
-  randomness: 0.2,
-  randomnessPower: 5,
-  insidecolor: "#ff6030",
-  outsidecolor: "#1b3984",
-};
+// todo 加载卡通材质
+textloader = new THREE.TextureLoader();
+const gradientMap = textloader.load("./img/5.jpg");
+gradientMap.magFilter = THREE.NearestFilter;
 
-// todo 星系初始化
-const initgalaxy = () => {
-  const positionarr = [];
-  const colorarr = [];
-  const insidecolor = new THREE.Color(parameters.insidecolor);
-  const outsidecolor = new THREE.Color(parameters.outsidecolor);
+// todo 添加物体
+const height_ratio = 4;
+const toonmaterial = new THREE.MeshToonMaterial({
+  color: "#fff",
+  gradientMap,
+});
 
-  const galaxygeometry = new THREE.BufferGeometry();
-  const galaxymaterial = new THREE.PointsMaterial({
-    size: parameters.size,
-    depthWrite: false,
-    sizeAttenuation: true,
-    vertexColors: true,
-  });
+const cone = new THREE.Mesh(
+  new THREE.ConeGeometry(0.8, 1.5, 32, 6),
+  toonmaterial
+);
+cone.position.set(2, -height_ratio * 0, 0);
 
-  for (let i = 0; i < parameters.num; i++) {
-    let index = i * 3;
+const tour = new THREE.Mesh(
+  new THREE.TorusGeometry(0.8, 0.2, 32),
+  toonmaterial
+);
+tour.position.set(-2, -height_ratio * 1, 0);
 
-    const radius = Math.random() * parameters.radius;
-    const spinAngel = radius * parameters.spin;
-    const angle = ((i % parameters.branch) / parameters.branch) * Math.PI * 2;
+const torus = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.6, 0.2, 64, 16),
+  toonmaterial
+);
+torus.position.set(2, -height_ratio * 2, 0);
 
-    const randomx =
-      Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
-    const randomy =
-      Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
-    const randomz =
-      Math.pow(Math.random(), parameters.randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1);
+scene.add(cone, tour, torus);
+modelsarr.push(cone, tour, torus);
 
-    positionarr[index] = radius * Math.sin(angle + spinAngel) + randomx;
-    positionarr[index + 1] = randomy;
-    positionarr[index + 2] = radius * Math.cos(angle + spinAngel) + randomz;
+// todo 添加粒子
+const vertices = 1000;
+const verticesarr = [];
+for (let i = 0; i < vertices * 3; i++) {
+  let index = i * 3;
+  verticesarr[index] = (Math.random() - 0.5) * 2 * 3;
+  verticesarr[index + 1] = (Math.random() - 0.5) * 2 * 10;
+  verticesarr[index + 2] = (Math.random() - 0.5) * 2 * 2;
+}
+const pointgeometry = new THREE.BufferGeometry();
+pointgeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(verticesarr, 3)
+);
 
-    const newcolor = insidecolor.clone();
-    newcolor.lerp(outsidecolor, radius / parameters.radius);
+const point = new THREE.Points(
+  pointgeometry,
+  new THREE.PointsMaterial({
+    size: 0.01,
+    color: "#fff",
+  })
+);
 
-    colorarr[index] = newcolor.r;
-    colorarr[index + 1] = newcolor.g;
-    colorarr[index + 2] = newcolor.b;
-  }
-  galaxygeometry.setAttribute(
-    "position",
-    new THREE.Float32BufferAttribute(positionarr, 3)
-  );
-  galaxygeometry.setAttribute(
-    "color",
-    new THREE.Float32BufferAttribute(colorarr, 3)
-  );
-  galaxy = new THREE.Points(galaxygeometry, galaxymaterial);
-  scene.add(galaxy);
-};
+scene.add(point);
 
-initgalaxy();
-
-// todo Gui
-gui
-  .add(parameters, "num")
-  .min(100)
-  .max(20000)
-  .step(3)
-  .name("星系数量")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    console.log(scene);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "size")
-  .min(0.1)
-  .max(1)
-  .step(0.1)
-  .name("星系大小")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "radius")
-  .min(0.1)
-  .max(10)
-  .step(0.1)
-  .name("星系半径")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "branch")
-  .min(3)
-  .max(12)
-  .step(1)
-  .name("星系分支")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "spin")
-  .min(0.2)
-  .max(2)
-  .step(0.1)
-  .name("星系弯曲度")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "randomness")
-  .min(0.2)
-  .max(2)
-  .step(0.1)
-  .name("星系弯曲度")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .add(parameters, "randomnessPower")
-  .min(5)
-  .max(10)
-  .step(1)
-  .name("星系弯曲度")
-  .onFinishChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .addColor(parameters, "insidecolor")
-  .name("内部颜色")
-  .onChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-gui
-  .addColor(parameters, "outsidecolor")
-  .name("外部颜色")
-  .onChange(() => {
-    scene.remove(galaxy);
-    initgalaxy();
-  });
-
-// ! 动画
+// ! 动画函数
 const clock = new THREE.Clock();
 const tick = () => {
-  const elapsed = clock.getElapsedTime();
   requestAnimationFrame(tick);
 
+  const delta = clock.getDelta();
+  modelsarr.forEach((item) => {
+    item.rotation.x += delta * 0.1;
+    item.rotation.y += delta * 0.5;
+    item.rotation.z += delta * 0.1;
+  });
+
   renderer.render(scene, camera);
-  controls.update();
 };
 
+// ! 初始化
 onMounted(() => {
   // todo 渲染器
-  controls = new OrbitControls(camera, canvas.value);
   renderer = new THREE.WebGLRenderer({
     canvas: canvas.value,
+    alpha: true,
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
-
-  // todo 执行动画
   tick();
+});
+
+// ! 滚动事件
+let first = 0;
+window.addEventListener("scroll", () => {
+  const scrolltop = scrollY / window.innerHeight;
+  const scrollround = Math.round(scrolltop);
+  if (scrollround !== first) {
+    first = scrollround;
+    gsap.to(modelsarr[first].rotation, {
+      duration: 1.5,
+      x: "+=1.5",
+      y: "+=2",
+      z: "+=0.5",
+    });
+  }
+  cameragroup.position.y = -scrolltop * height_ratio;
+});
+
+// ! 移动事件
+window.addEventListener("mousemove", (e) => {
+  const mouseposition = {
+    x: (e.pageX / window.innerWidth - 0.5) * 2,
+    y: -(e.clientY / window.innerHeight - 0.5) * 2,
+  };
+  camera.position.set(mouseposition.x * 0.2, mouseposition.y * 0.2, 5);
+});
+
+// ! 自适应
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
 </script>
 
 <template>
   <canvas id="canvas" ref="canvas"></canvas>
+  <section id="main">
+    <li>
+      <h3>MY PORTFOLIO</h3>
+    </li>
+    <li>
+      <h3>CALL MY PHONE</h3>
+    </li>
+    <li>
+      <h3>CONTACT ME</h3>
+    </li>
+  </section>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 #canvas {
-  position: absolute;
+  position: fixed;
+  top: 0;
+}
+
+#main {
+  li {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    h3 {
+      color: #fff;
+      font-size: 4.7917vw;
+    }
+    &:nth-of-type(2n-1) {
+      justify-content: left;
+      h3 {
+        margin-left: 5.2083vw;
+      }
+    }
+    &:nth-of-type(2n) {
+      justify-content: right;
+      h3 {
+        margin-right: 5.2083vw;
+      }
+    }
+  }
 }
 </style>
