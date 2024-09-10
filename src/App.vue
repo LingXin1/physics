524e5d1a899586da3b09de7a28969ae6efcb29c9
wangrenzhen <974993442@qq.com>
2024-09-10 09:16:07 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,207 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// ? 物理库
+import * as CANNON from "cannon-es";
+
 import GUI from "lil-gui";
 import gsap from "gsap";
 
 const canvas = ref(null);
-const gii = new GUI();
 
-let scene, camera, renderer, textloader;
-let modelsarr = [];
+let scene, world, camera, renderer, control;
 
-// ! 场景
+const audio = new Audio("./audio/hit.mp3");
+
+// ! 场景与物理
 scene = new THREE.Scene();
+world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0),
+  allowSleep: true,
+});
+world.broadphase = new CANNON.SAPBroadphase(world);
+const base = new CANNON.Material();
+world.defaultContactMaterial = new CANNON.ContactMaterial(base, base, {
+  friction: 0.1,
+  restitution: 0.8,
+});
+scene.add(new THREE.AxesHelper(1));
+
+// ! 添加球体函数
+const addsphere = () => {
+  // ? 球体半径
+  const radius = Math.random() * (0.5 - 0.1);
+  // ? 球体位置
+  const sphereX = Math.random() * (1 + 1) - 1;
+  const sphereZ = Math.random() * (1 + 1) - 1;
+
+  spheres(radius, { x: sphereX, y: 4, z: sphereZ });
+};
+// ! 添加盒子
+const addbox = () => {
+  // ? 盒子大小
+  const size = Math.random() * (0.8 - 0.1);
+  // ? 盒子位置
+  const sphereX = Math.random() * (1 + 1) - 1;
+  const sphereZ = Math.random() * (1 + 1) - 1;
+  boxs(size, { x: sphereX, y: 4, z: sphereZ });
+};
+
+const fun = {
+  addsphere,
+  addbox,
+};
+const gui = new GUI();
+gui.add(fun, "addsphere");
+gui.add(fun, "addbox");
 
 // ! 灯光
 const directional = new THREE.DirectionalLight("#fff", 2);
-directional.position.set(1, 1, 0);
+directional.castShadow = true;
+directional.shadow.mapSize.set(2048, 2048);
+directional.shadow.camera.far = 20;
+directional.position.set(5, 5, 5);
 scene.add(directional);
 
 // ! 相机
-const cameragroup = new THREE.Group();
 camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(0, 0, 5);
-cameragroup.add(camera);
-scene.add(cameragroup);
+camera.position.set(5, 5, 5);
 
-// todo 加载卡通材质
-textloader = new THREE.TextureLoader();
-const gradientMap = textloader.load("./img/5.jpg");
-gradientMap.magFilter = THREE.NearestFilter;
-
-// todo 添加物体
-const height_ratio = 4;
-const toonmaterial = new THREE.MeshToonMaterial({
-  color: "#fff",
-  gradientMap,
-});
-
-const cone = new THREE.Mesh(
-  new THREE.ConeGeometry(0.8, 1.5, 32, 6),
-  toonmaterial
-);
-cone.position.set(2, -height_ratio * 0, 0);
-
-const tour = new THREE.Mesh(
-  new THREE.TorusGeometry(0.8, 0.2, 32),
-  toonmaterial
-);
-tour.position.set(-2, -height_ratio * 1, 0);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(0.6, 0.2, 64, 16),
-  toonmaterial
-);
-torus.position.set(2, -height_ratio * 2, 0);
-
-scene.add(cone, tour, torus);
-modelsarr.push(cone, tour, torus);
-
-// todo 添加粒子
-const vertices = 1000;
-const verticesarr = [];
-for (let i = 0; i < vertices * 3; i++) {
-  let index = i * 3;
-  verticesarr[index] = (Math.random() - 0.5) * 2 * 3;
-  verticesarr[index + 1] = (Math.random() - 0.5) * 2 * 10;
-  verticesarr[index + 2] = (Math.random() - 0.5) * 2 * 2;
-}
-const pointgeometry = new THREE.BufferGeometry();
-pointgeometry.setAttribute(
-  "position",
-  new THREE.Float32BufferAttribute(verticesarr, 3)
-);
-
-const point = new THREE.Points(
-  pointgeometry,
-  new THREE.PointsMaterial({
-    size: 0.01,
-    color: "#fff",
+// ! 地面
+const paln = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
   })
 );
+paln.receiveShadow = true;
+paln.rotation.x = -Math.PI / 2;
 
-scene.add(point);
+const physicsplan = new CANNON.Body({
+  mass: 0,
+  shape: new CANNON.Plane(),
+});
+physicsplan.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+world.addBody(physicsplan);
+scene.add(paln);
+
+const shapes = [];
+// ! 球体函数
+const spheres = (radius, position) => {
+  // todo 创造球
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(radius, 64, 32),
+    new THREE.MeshStandardMaterial({
+      roughness: 0.4,
+      metalness: 0.6,
+    })
+  );
+  sphere.castShadow = true;
+  sphere.position.set(position.x, position.y, position.z);
+  // todo 创造物理
+
+  const physicssphere = new CANNON.Body({
+    mass: 0.5,
+    shape: new CANNON.Sphere(radius),
+    position: new CANNON.Vec3(position.x, position.y, position.z),
+  });
+
+  physicsplan.addEventListener("collide", (e) => {
+    const impact = e.contact.getImpactVelocityAlongNormal();
+    if (impact > 1.5) {
+      audio.volume = Math.random();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  });
+
+  world.addBody(physicssphere);
+  scene.add(sphere);
+
+  shapes.push({
+    shape: sphere,
+    physicsshape: physicssphere,
+  });
+};
+
+const boxss = [];
+const boxs = (size, position) => {
+  // todo 创造盒子
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(size, size, size),
+    new THREE.MeshStandardMaterial({
+      roughness: 0.4,
+      metalness: 0.6,
+    })
+  );
+  box.castShadow = true;
+  box.position.set(position.x, position.y, position.z);
+  // todo 创造物理
+
+  const physicsbox = new CANNON.Body({
+    mass: 0.5,
+    shape: new CANNON.Box(new CANNON.Vec3(size / 2, size / 2, size / 2)),
+    position: new CANNON.Vec3(position.x, position.y, position.z),
+  });
+
+  physicsbox.addEventListener("collide", (e) => {
+    const impact = e.contact.getImpactVelocityAlongNormal();
+    if (impact > 1.5) {
+      audio.volume = Math.random();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  });
+
+  world.addBody(physicsbox);
+  scene.add(box);
+
+  boxss.push({
+    shape: box,
+    physicsshape: physicsbox,
+  });
+};
 
 // ! 动画函数
 const clock = new THREE.Clock();
 const tick = () => {
-  requestAnimationFrame(tick);
-
   const delta = clock.getDelta();
-  modelsarr.forEach((item) => {
-    item.rotation.x += delta * 0.1;
-    item.rotation.y += delta * 0.5;
-    item.rotation.z += delta * 0.1;
-  });
+  requestAnimationFrame(tick);
+  control.update();
 
+  world.step(1 / 60, delta, 3);
+  shapes.forEach((item) => {
+    item.shape.position.copy(item.physicsshape.position);
+  });
+  boxss.forEach((item) => {
+    item.shape.position.copy(item.physicsshape.position);
+    item.shape.quaternion.copy(item.physicsshape.quaternion);
+  });
   renderer.render(scene, camera);
 };
 
 // ! 初始化
 onMounted(() => {
+  // ! 轨道控制器
+  control = new OrbitControls(camera, canvas.value);
   // todo 渲染器
   renderer = new THREE.WebGLRenderer({
     canvas: canvas.value,
     alpha: true,
+    antialias: true,
   });
+  renderer.shadowMap.enabled = true;
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
   tick();
-});
-
-// ! 滚动事件
-let first = 0;
-window.addEventListener("scroll", () => {
-  const scrolltop = scrollY / window.innerHeight;
-  const scrollround = Math.round(scrolltop);
-  if (scrollround !== first) {
-    first = scrollround;
-    gsap.to(modelsarr[first].rotation, {
-      duration: 1.5,
-      x: "+=1.5",
-      y: "+=2",
-      z: "+=0.5",
-    });
-  }
-  cameragroup.position.y = -scrolltop * height_ratio;
-});
-
-// ! 移动事件
-window.addEventListener("mousemove", (e) => {
-  const mouseposition = {
-    x: (e.pageX / window.innerWidth - 0.5) * 2,
-    y: -(e.clientY / window.innerHeight - 0.5) * 2,
-  };
-  camera.position.set(mouseposition.x * 0.2, mouseposition.y * 0.2, 5);
 });
 
 // ! 自适应
@@ -151,47 +214,11 @@ window.addEventListener("resize", () => {
 
 <template>
   <canvas id="canvas" ref="canvas"></canvas>
-  <section id="main">
-    <li>
-      <h3>MY PORTFOLIO</h3>
-    </li>
-    <li>
-      <h3>CALL MY PHONE</h3>
-    </li>
-    <li>
-      <h3>CONTACT ME</h3>
-    </li>
-  </section>
 </template>
 
 <style scoped lang="less">
 #canvas {
   position: fixed;
   top: 0;
-}
-
-#main {
-  li {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    h3 {
-      color: #fff;
-      font-size: 4.7917vw;
-    }
-    &:nth-of-type(2n-1) {
-      justify-content: left;
-      h3 {
-        margin-left: 5.2083vw;
-      }
-    }
-    &:nth-of-type(2n) {
-      justify-content: right;
-      h3 {
-        margin-right: 5.2083vw;
-      }
-    }
-  }
 }
 </style>
